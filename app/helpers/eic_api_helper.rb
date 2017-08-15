2de9a69d6050071ebda0b4b375e69b33789d3f93
Shawn Deprey <shawndeprey@Shawns-MacBook-Pro.local>
2017-08-14 21:43:36 -0600

    Added URI encoding for passwords

--- app/helpers/eic_api_helper.rb
+++ app/helpers/eic_api_helper.rb
@@ -1,13 +1,13 @@
+require 'uri'
 module EicApiHelper
 	EIC_DOMAIN = "http://eicgaming.com"
 
   def self.get_user(username, password)
   	return if username.blank? || password.blank?
-    return HttpHelper.get("#{EicApiHelper::EIC_DOMAIN}/api/v1/users.php?username=#{username}&password=#{password}", true)
+    return HttpHelper.get("#{EicApiHelper::EIC_DOMAIN}/api/v1/users.php?username=#{username}&password=#{URI.encode(password)}", true)
   end
 
   def self.get_user_from_payload(payload)
-  	# {"username"=>"Drieton", "avatar"=>"http://eicgaming.com/wp-content/uploads/ultimatemember/54/profile_photo-40.jpg?1502763275", "rank"=>"CMDR Class I", "tags"=>["bgs-nerd", "sales-nerd"]}
   	user = User.find_by_username(payload["user"]["username"]) || User.new
     user.username = payload["user"]["username"]
     user.image_url = payload["user"]["avatar"] unless payload["user"]["avatar"].blank?
