11feae9d101de979087ea70348626158b25b9a63
Shawn Deprey <shawndeprey@gmail.com>
2018-01-06 10:47:30 -0700

    Make sure only admins can download the orders csv

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,7 +17,9 @@ class OrdersController < ApplicationController
     @orders = Order.where(query).order('created_at desc').limit(500)
     respond_to do |format|
       format.html
-      # format.csv
+      if @session_user.admin?
+        format.csv
+      end
     end
   end
 
