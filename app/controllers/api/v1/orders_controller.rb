b29e1bd20830b8d9f06312180c905680ce81dc60
Cazz0r <root@vps142599.vps.ovh.ca>
2018-04-26 04:06:26 +0000

    Trying to find the correct way to use POST data

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::OrdersController < Api::V1::BaseController
     return custom_error([:account_required]) unless order_params && !order_params[:account_id].blank?
     @order = Order.new(order_params)
     return ar_error(@order) unless @order.save
-    TradeEvent.new({content: params.initial_note, user_id: @session_user.id, order_id: @order.id, account_id: @order.account_id}).save
+    TradeEvent.new({content: order_note[:initial_note], user_id: @session_user.id, order_id: @order.id, account_id: @order.account_id}).save
     remder_order(201)
   end
 
@@ -33,6 +33,10 @@ class Api::V1::OrdersController < Api::V1::BaseController
   private
   def order_params
     params.require(:order).permit(:description, :account_id, :user_id, :time_window, :order_type, :status, :platform, :order_quantity, :order_commodity)
+  end
+
+  def order_note
+    params.require(:event).permit(:initial_note)
   end
 
   def render_orders(status)
