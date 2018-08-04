d282851599bbdb1cf9f4dcab4806890caddb9519
Shawn Deprey <shawndeprey@gmail.com>
2018-08-04 00:58:59 -0600

    Update new VM build

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,5 @@
 EIC_URL = "https://eicgaming.com"
-GENERATOR_ADDITION = ENV['GENERATOR_ADDITION']
+GENERATOR_ADDITION = "kbnviubnrtni4i4nvui88yu8hfduivbh>efvf?efrsrvr<eferveefe&efrrvdvrdf@dvfrevsd-xeFEsvfd"
 
 # Order constants
 ORDER_OPEN, ORDER_UNDERWAY, ORDER_CLOSED, ORDER_CANCELED = 0, 1, 2, 3
